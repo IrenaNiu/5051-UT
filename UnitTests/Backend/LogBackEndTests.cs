9e3ff8acc7cad9452d1229b96ea8231b370c8565
Yi Niu <niuyi@seattleu.edu>
2019-06-10 00:14:53 -0700

    Added LogBackendTests.cs to the project

---
+++ UnitTests/Backend/LogBackEndTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HW1c.Backend;
+using HW1c.Models;
+using System.Linq;
+
+namespace UnitTests.Backend
+{
+    [TestClass]
+    public class LogBackendTests
+    {
+       
+
+
+    }
+}
