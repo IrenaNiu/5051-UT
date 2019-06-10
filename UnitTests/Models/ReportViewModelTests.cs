a00c9295d01c7934624cf355e47812ae837c2cad
Yi Niu <niuyi@seattleu.edu>
2019-06-09 22:54:23 -0700

    Added ReportViewModel LogViewModel Get Test

--- UnitTests/Models/ReportViewModelTests.cs
+++ UnitTests/Models/ReportViewModelTests.cs
@@ -48,5 +48,17 @@ namespace UnitTests.Models
             Assert.AreEqual(5, result);
         }
 
+        [TestMethod]
+        public void ReportViewModel_Get_Default_Should_Pass()
+        {
+            // Arrange 
+            var myTest = new ReportViewModel();
+
+            // Act 
+            var result = myTest.LogViewModel;
+
+            // Assert 
+            Assert.IsNotNull(result);
+        }
     }
 }
