c580a188b0e39a5d36f857bae390fb3cc15d6e78
Yi Niu <niuyi@seattleu.edu>
2019-06-10 23:29:48 -0700

    Added LogBackend_Create_Valid_Item_Should_Pass() test case

--- UnitTests/Backend/LogBackEndTests.cs
+++ UnitTests/Backend/LogBackEndTests.cs
@@ -133,6 +133,28 @@ namespace UnitTests.Backend
             Assert.AreEqual(null, newItem);
         }
 
+        [TestMethod]
+        public void LogBackend_Create_Valid_Item_Should_Pass()
+        {
+            // Arange
 
+            // Get the first item from the list
+            var oldItem = new LogModel();
+            oldItem.ID = "ID";
+            oldItem.PhoneID = "PhoneID";
+            oldItem.RecordedDateTime = DateTime.Parse("01/23/2019");
+            oldItem.Value = "Value";
+
+            // Act
+            var newItem = LogBackend.Instance.Create(oldItem);
+
+            // Check each item one by one to ensure it is correctly loaded
+
+            // Assert
+            Assert.AreEqual(oldItem.ID, newItem.ID);
+            Assert.AreEqual(oldItem.PhoneID, newItem.PhoneID);
+            Assert.AreEqual(oldItem.RecordedDateTime, newItem.RecordedDateTime);
+            Assert.AreEqual(oldItem.Value, newItem.Value);
+        }
     }
 }
