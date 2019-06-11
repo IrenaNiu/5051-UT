e763925f9768555fd3d87ad9e43e7bbe73c08640
Yi Niu <niuyi@seattleu.edu>
2019-06-10 21:49:23 -0700

    Added LogBackend Index test Case

--- UnitTests/Backend/LogBackEndTests.cs
+++ UnitTests/Backend/LogBackEndTests.cs
@@ -9,7 +9,22 @@ namespace UnitTests.Backend
     [TestClass]
     public class LogBackendTests
     {
-       
+        [TestMethod]
+        public void LogBackEnd_Index_Default_Should_Pass()
+        {
+            // Should load the dataset with 4 rows
+
+            // Arange            
+            var myViewModel = new LogViewModel();
+
+            // Act
+            myViewModel = LogBackend.Instance.Index();
+            var result = myViewModel.LogList;
+
+            // Assert
+            Assert.AreEqual(4, result.Count);
+        }
+
 
 
     }
